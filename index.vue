2e4f7f3221317f5e303981e3c9a37f3f019f2c18
🐶Bolt <bolt@wangchunyandeMacBook-Pro-4.local>
2017-12-11 17:57:56 +0800

    update

--- index.vue
+++ index.vue
@@ -1,3 +1,17 @@
 <template>
     <div>Hello World!</div>
 </template>
+
+<style lang="less">
+    @color: blue;
+
+    div {
+        color: @color;
+    }
+</style>
+
+<style scoped>
+    div {
+        font-size: 32px;
+    }
+</style>
